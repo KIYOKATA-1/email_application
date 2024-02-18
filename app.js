16a9611bfa8eb8714e2971a2f61e8403e44b1612
Pavel <112262251+KIYOKATA-1@users.noreply.github.com>
2024-02-18 07:07:37 +0600

    creacting http request

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const path = require('path');
+const nodemailer = require('nodemailer');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 //Middleware
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/app.html'));
 });
 
+app.post('/', (req, res) => {
+    console.log(req.body);
+
+})
 
 app.listen(PORT, () =>{
     console.log(`app listening on port ${PORT}`)
